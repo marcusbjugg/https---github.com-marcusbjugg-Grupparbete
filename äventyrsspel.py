6f21cbb2b5afde9af6b507af515eb9d8f8fc5722
marcusbjugg <marcus.bjuggren@skola.taby.se>
2025-01-16 16:20:04 +0100

    uppdatering

--- äventyrsspel.py
+++ äventyrsspel.py
@@ -5,34 +5,69 @@ Game = True
 
 
 #Egenskaper
-STK = 5
-HP = 10
+STK = [5]
+HP = [10]
 LVL = 1
 
 
 weapons = {
-    1: ["Kniv", "Hammare", "Nål", "Penna", "Rep"],
-    2: ["Svärd", "Pilbåge", "Yxa"],
-    3: ["Glock", "Shotgun", "Granat"],
-    4: ["Kalaschnikov", "Eldkastare"],
-    5: ["Tank"]
+    1: ["kniv", "hammare", "nål", "penna", "rep"],
+    2: ["svärd", "pilbåge", "yxa"],
+    3: ["glock", "shotgun", "granat"],
+    4: ["kalaschnikov", "eldkastare"],
+    5: ["tank"]
 }
 
 ryggsäck = []
 max_backpack_size = 5    
 
+def styrka_remove(remove):
+    styrka_tabort = 0
+    remove = remove.lower()
+    if remove == ("kniv", "hammare", "nål", "penna", "rep"):
+        styrka_tabort = 1
+    elif remove == ("svärd", "pilbåge", "yxa"):
+        styrka_tabort = 2
+    elif remove == ("glock", "shotgun", "granat"):
+        styrka_tabort = 3
+    elif remove == ("kalaschnikov", "eldkastare"):
+        styrka_tabort = 4
+    elif remove == ("tank"):
+        styrka_tabort = 5
+    return styrka_tabort
+
 def get_random_weapon():
     strength_level = random.randint(1, 6)
+    strength = strength_level
     weapon_name = random.choice(weapons[strength_level])
-    return weapon_name, strength_level
+    weapon = weapon_name
+    return weapon, strength
 
 def add_weapon_to_ryggsäck(weapon_name,  strength_level):
+    if len(ryggsäck) != max_backpack_size:
+        ryggsäck.append(weapon_name)
+        STK.append(strength_level)
+    else:
+        print(ryggsäck)
+        remove = input("Ange vad i ryggsäcken du vill ta bort -> ").lower()
+        print(remove)
+        if remove not in ryggsäck:
+            print("Värdet finns inte i listan")
+        else:
+            ryggsäck.remove(remove)
+            STK.remove(styrka_remove(remove))
+            print("Nu har,", remove,"tagits bort från listan.\nNu ser listan ut så här\n", ryggsäck, STK)
     
-    return ryggsäck, visa_egenskaper
+    return ryggsäck
+p = range(0,7)
+
+for i in p:
+    my_weapon = get_random_weapon()
+    add_weapon_to_ryggsäck(my_weapon[0], my_weapon[1])
+    print(STK)
 
 
-
-visa_egenskaper = (f"STK: {STK}\nHP: {HP}\nLVL: {LVL}")
+visa_egenskaper = (f"STK: {sum(STK)}\nHP: {sum(HP)}\nLVL: {LVL}")
 visa_invetory = (f"{ryggsäck}")
 
 #Funktion för att välja dörr
